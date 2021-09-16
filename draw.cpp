6b03e57ad8b6c0cc3b82d198d51ef9c17e1c45ad
Hamad Al Marri <hamad.s.almarri@gmail.com>
2021-09-16 21:40:55 +0300

    mpfr

--- draw.cpp
+++ draw.cpp
@@ -4,12 +4,14 @@
 #include <stdio.h>
 #include <cmath>
 #include <pthread.h>
+#include <chrono>
+#include <mpfr.h>
 
 #define R 0
 #define G 1
 #define B 2
 
-#define MAX_ITER 250 //100
+#define MAX_ITER 250 //250 //100
 
 #define YSIZE 1200
 #define PROPOTION 1.666666667
@@ -21,7 +23,7 @@
 #define RIGHT_SHIFT 0
 #define TOP_SHIFT 0
 
-// #define UPDATE_RATE 0.016 // 62fps
+#define MPFR_PRECISION 200 // 200
 
 class MyWindow;
 
@@ -34,10 +36,10 @@ struct thread_data {
 class MyWindow : public Fl_Double_Window {
 private:
 	unsigned char pixbuf[YSIZE][XSIZE][3];
-	double zoomX;
-	double zoomY;
-	double origX;
-	double origY;
+	mpfr_t zoomX;
+	mpfr_t zoomY;
+	mpfr_t origX;
+	mpfr_t origY;
 
 	// FLTK DRAW METHOD
 	void draw() {
@@ -50,37 +52,58 @@ public:
 	{
 		end();
 		
-		zoomX = SCALE_X;
-		zoomY = SCALE_Y;
-		origX = RIGHT_SHIFT;
-		origY = TOP_SHIFT;
+		mpfr_init2(zoomX, MPFR_PRECISION);
+		mpfr_init2(zoomY, MPFR_PRECISION);
+		mpfr_init2(origX, MPFR_PRECISION);
+		mpfr_init2(origY, MPFR_PRECISION);
+
+		mpfr_set_d(zoomX, SCALE_X, MPFR_RNDD);
+		mpfr_set_d(zoomY, SCALE_Y, MPFR_RNDD);
+		mpfr_set_d(origX, RIGHT_SHIFT, MPFR_RNDD);
+		mpfr_set_d(origY, TOP_SHIFT, MPFR_RNDD);
 	
 		RenderImage();
 	}
+	
+	~MyWindow() {
+		mpfr_clear(zoomX);
+		mpfr_clear(zoomY);
+		mpfr_clear(origX);
+		mpfr_clear(origY);
+
+		mpfr_free_cache();
+	}
 
 	int handle(int e) {
-		if (e == FL_RELEASE) {
-			// double newX = convertXAxis(Fl::event_x());
-			double newX = Fl::event_x() - ((double) XSIZE / 2.0);
-			double newY = Fl::event_y() - ((double) YSIZE / 2.0);
-			
-			newX = zoomX * newX / (double) XSIZE;
-			newY = zoomY * newY / (double) YSIZE;
-			
-			// printf("x: %d, y: %d\n", Fl::event_x(), Fl::event_y());			
-			// printf("new x: %f, new y: %f\n", newX, newY);
+		mpfr_t newX, newY, temp;
 
-			origX += newX;
-			origY += newY;
-			// printf("new origX: %f\n", this->origX);
+		if (e != FL_RELEASE)
+			return 0;
 
-			zoomX -= zoomX / (SCALE_X - 2.0);
-			zoomY = zoomX / PROPOTION;
-			
-			// printf("zoom: %lf\n", this->zoomX);
+		mpfr_init2(newX, MPFR_PRECISION);
+		mpfr_init2(newY, MPFR_PRECISION);
+		mpfr_init2(temp, MPFR_PRECISION);
 
-			RenderImage();
+		mpfr_set_d(newX, Fl::event_x() - ((double) XSIZE / 2.0), MPFR_RNDD);
+		mpfr_set_d(newY, Fl::event_y() - ((double) YSIZE / 2.0), MPFR_RNDD);
+
+		mpfr_mul(newX, newX, zoomX, MPFR_RNDD);
+		mpfr_div_d(newX, newX, (double) XSIZE, MPFR_RNDD);
+
+		mpfr_mul(newY, newY, zoomY, MPFR_RNDD);
+		mpfr_div_d(newY, newY, (double) YSIZE, MPFR_RNDD);
+
+		mpfr_add(origX, origX, newX, MPFR_RNDD);
+		mpfr_add(origY, origY, newY, MPFR_RNDD);
+
+		for (int i = 0; i < 4; ++i) {
+			mpfr_div_2ui(temp, zoomX, (SCALE_X - 2.0), MPFR_RNDD);
+			mpfr_sub(zoomX, zoomX, temp, MPFR_RNDD);
 		}
+
+		mpfr_div_d(zoomY, zoomX, PROPOTION, MPFR_RNDD);
+		
+		RenderImage();
 
 		return 0;
 	}
@@ -92,90 +115,96 @@ public:
 		pixbuf[y][x][B] = b;
 	}
 
-	double convertXAxis(unsigned int x) {
-		double ecX = x;
-		ecX = ecX - ((double) XSIZE / 2.0);
-		ecX = zoomX * ecX / (double) XSIZE;
-		ecX += origX;
-		return ecX;
+	void convertXAxis(mpfr_t &ecX, unsigned int x) {
+		mpfr_set_d(ecX, x, MPFR_RNDD);
+		mpfr_sub_d(ecX, ecX, (double) XSIZE / 2.0, MPFR_RNDD);
+		mpfr_mul(ecX, ecX, zoomX, MPFR_RNDD);
+		mpfr_div_d(ecX, ecX, (double) XSIZE, MPFR_RNDD);
+		mpfr_add(ecX, ecX, origX, MPFR_RNDD);
 	}
 
-	double convertYAxis(unsigned int y) {
-		double ecY = y;
-		ecY = ecY - ((double) YSIZE / 2.0);
-		ecY = zoomY * ecY / (double) YSIZE;
-		ecY += origY;
-		return ecY;
+	void convertYAxis(mpfr_t &ecY, unsigned int y) {
+		mpfr_set_d(ecY, y, MPFR_RNDD);
+		mpfr_sub_d(ecY, ecY, (double) YSIZE / 2.0, MPFR_RNDD);
+		mpfr_mul(ecY, ecY, zoomY, MPFR_RNDD);
+		mpfr_div_d(ecY, ecY, (double) YSIZE, MPFR_RNDD);
+		mpfr_add(ecY, ecY, origY, MPFR_RNDD);
 	}
 
-	unsigned int mandelbrot(unsigned int x, unsigned int y, double &zR, double &zI) {		
+	unsigned int mandelbrot(unsigned int x, unsigned int y, mpfr_t &zR, mpfr_t &zI) {		
 		unsigned int iter = 0;
-		double cR, cI, old_zR;
-		
-		zR = zI = 0.0;
-		cR = convertXAxis(x);
-		cI = convertYAxis(y);
-		
-		// printf("cR: %f, cI: %f ", cR, cI);
+		mpfr_t cR, cI, old_zR, temp;
+
+		mpfr_init2(cR, MPFR_PRECISION);
+		mpfr_init2(cI, MPFR_PRECISION);
+		mpfr_init2(old_zR, MPFR_PRECISION);
+		mpfr_init2(temp, MPFR_PRECISION);
+
+		mpfr_set_d(zR, 0.0, MPFR_RNDD);
+		mpfr_set_d(zI, 0.0, MPFR_RNDD);
+
+		convertXAxis(cR, x);
+		convertYAxis(cI, y);
 
 		do {
-			old_zR = zR;
-			// zR = (zR * zR) - (zI * zI) + cR;
-			// zI = (2.0 * old_zR * zI) + cI;
+			mpfr_set(old_zR, zR, MPFR_RNDD);
 
-			zR = (zR * zR) - (zI * zI) + cR;
-			zI = (2.0 * old_zR * zI) + cI;
-			
+			// zR = (zR * zR) - (zI * zI) + cR;			
+			mpfr_mul(zR, zR, zR, MPFR_RNDD);
+			mpfr_mul(temp, zI, zI, MPFR_RNDD);
+			mpfr_sub(zR, zR, temp, MPFR_RNDD);
+			mpfr_add(zR, zR, cR, MPFR_RNDD);
+
+			// zI = (2.0 * old_zR * zI) + cI;
+			mpfr_mul_d(zI, zI, 2.0, MPFR_RNDD);
+			mpfr_mul(zI, zI, old_zR, MPFR_RNDD);
+			mpfr_add(zI, zI, cI, MPFR_RNDD);
+
 			iter++;
-		} while ((zR * zR) + (zI * zI) < 4.0 && iter < MAX_ITER);
+			
+			mpfr_mul(temp, zR, zR, MPFR_RNDD);
+			mpfr_mul(old_zR, zI, zI, MPFR_RNDD);
+			mpfr_add(temp, temp, old_zR, MPFR_RNDD);
+
+		// } while ((zR * zR) + (zI * zI) < 4.0 && iter < MAX_ITER);
+		} while (mpfr_cmp_d(temp, 4.0) < 0 && iter < MAX_ITER);
+
+		mpfr_clear(cR);
+		mpfr_clear(cI);
+		mpfr_clear(old_zR);
+		mpfr_clear(temp);
 
 		return iter;
-	}
-	
-	void colorize(unsigned char &r, unsigned char &g, unsigned char &b
-			, double zR, double zI)
-	{
-		// printf("zR: %f, zI: %f\n", zR, zI);
-		unsigned int scale = 100000;
-		double max = 4.0 * scale;
-
-		zR = abs(zR * scale);
-		zI = abs(zI * scale);
-
-		r = 184 * log(zR) / log(scale * 2);
-		g = 184 * log(zI) / log(scale * 2);
-		b = 204 * log(sqrt(zR * zR + zI * zI)) / log(sqrt(scale * scale + scale * scale));
-		// printf("r: %d, g: %d, b: %d\n", r, g, b);
 	}
 
 	// MAKE A NEW PICTURE IN THE PIXEL BUFFER, SCHEDULE FLTK TO DRAW IT
 	void _RenderImage(unsigned int from, unsigned int to) {
 		unsigned char r, g, b;
 		unsigned int iter;
-		double zR, zI;
+		mpfr_t zR, zI;
 		int scale_iter = 2;
+
+		mpfr_init2(zR, MPFR_PRECISION);
+		mpfr_init2(zI, MPFR_PRECISION);
 
 		for (unsigned int y = from; y < to; y++) {
 			for (unsigned int x = 0; x < XSIZE; x++) {
-				iter = MyWindow::mandelbrot(x, y, zR, zI);
-				
+				iter = mandelbrot(x, y, zR, zI);
+
+				r = g = b = 0;
+
 				if (iter != MAX_ITER) {
 					r = g = b = 0;
 					iter *= scale_iter;
-					b = (254 * log(iter) / log(MAX_ITER * scale_iter));
-					r = (50 * log(iter * abs(zR)) / log(MAX_ITER * scale_iter));
-					g = (50 * log(iter * abs(zI)) / log(MAX_ITER * scale_iter));
-					// printf("log(%d): %f, b: %d\n", iter, log(iter), b);
-					
-				} else {
-					colorize(r, g, b, zR, zI);
-					// r = g = 0;
-					// b = 254;
+					b = (254 * log(iter) / log(MAX_ITER * scale_iter));					
 				}
 
 				PlotPixel(x, y, r, g, b);
 			}
 		}
+
+		mpfr_clear(zR);
+		mpfr_clear(zI);
 	}
 
 	static void *thread_wraper(void *context) {
@@ -186,7 +215,6 @@ public:
 	}
 
 	void RenderImage() {
-		// Fl::lock();
 		const int number_of_threads = 4;
 		pthread_t threads[number_of_threads];
 		struct thread_data td[number_of_threads];
@@ -194,6 +222,8 @@ public:
 		const unsigned int step = YSIZE / number_of_threads;
 		unsigned int to = step;
 
+		auto started = std::chrono::high_resolution_clock::now();
+		
 		for (int i = 0; i < number_of_threads; ++i) {
 			td[i].from = from;
 			td[i].to = to;
@@ -207,6 +237,10 @@ public:
 
 		for (int i = 0; i < number_of_threads; ++i)
 			pthread_join(threads[i], NULL);
+
+		auto done = std::chrono::high_resolution_clock::now();
+		printf("took: %ldms\n",
+			std::chrono::duration_cast<std::chrono::milliseconds>(done - started).count());
 
 		redraw();
 	}
